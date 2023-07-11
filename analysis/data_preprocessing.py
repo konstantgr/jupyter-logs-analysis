e2a19840c8aaca0ec250331ee3c9992773a26d3a
konstantgr <konstantin.grotov@gmail.com>
2023-07-11 20:11:21 +0200

    Modified transitions processing

--- analysis/data_preprocessing.py
+++ analysis/data_preprocessing.py
@@ -1,15 +1,17 @@
 import ast
-
 import pandas as pd
+
 import code_diff as cd
 
 from pathlib import Path
+from collections import defaultdict
+from typing import List, Dict
 
+from tqdm import tqdm
 from radon.raw import analyze
 from radon.visitors import ComplexityVisitor
-from tqdm import tqdm
+
 from analysis.data_loading import read_hackathon_data
-from collections import defaultdict
 
 
 class MetricsProcessor:
@@ -22,7 +24,7 @@ class MetricsProcessor:
         }
         self.metrics_dataframes = defaultdict()
 
-    def _get_ast(self, source: str) -> ast.AST:
+    def _get_ast(self, source: str | None) -> ast.AST:
         try:
             return ast.parse(source)
         except SyntaxError as e:
@@ -31,7 +33,15 @@ class MetricsProcessor:
             code_string = '\n'.join(code_string)
             return self._get_ast(code_string)
 
-    def get_objects_number(self, source: str | None):
+    @staticmethod
+    def _get_code_changes(prev: str | None, cur: str | None) -> List:
+        try:
+            output = cd.difference(prev, cur, lang="python")
+            return output.edit_script()
+        except ValueError:
+            return []
+
+    def get_objects_number(self, source: str | None) -> int:
         if source is None:
             return 0
         source_ast, variables = self._get_ast(source), set()
@@ -42,7 +52,7 @@ class MetricsProcessor:
 
         return len(variables)
 
-    def get_cyclomatic_complexity(self, source: str | None):
+    def get_cyclomatic_complexity(self, source: str | None) -> int:
         if source is None:
             return 0
 
@@ -59,31 +69,31 @@ class MetricsProcessor:
         except SyntaxError:
             return len(source.splitlines())
 
-    @staticmethod
-    def _get_code_changes(prev, cur):
-        try:
-            output = cd.difference(prev, cur, lang="python")
-            return output.edit_script()
-        except ValueError:
-            return []
+    def get_kernel_transitions(self, kernel_id: str, df: pd.DataFrame) -> List[Dict]:
+        transitions = []
+        cols = ['cell_index', 'cell_source', 'cell_num']
+        executions_list = df[cols].to_numpy()
+
+        prev_idx, prev_source, prev_num = executions_list[0]
+        for (idx, source, num) in executions_list[1:]:
+            code_changes = self._get_code_changes(prev_source, source) if idx == prev_idx else []
+            transitions.append({
+                'kernel_id': kernel_id,
+                'cell_idx_from': prev_idx, 'cell_num_from': prev_num, 'cell_source_from': prev_source,
+                'cell_idx_to': idx, 'cell_num_to': num, 'cell_source_to': source,
+                'inner_transition': idx == prev_idx,
+                'changes': code_changes
+            })
+            prev_idx, prev_source, prev_num = idx, source, num
+
+        return transitions
 
     def get_execution_transitions(self, df: pd.DataFrame) -> pd.DataFrame:
         grouped = df.groupby('event').get_group('execute').groupby('kernel_id')
 
         transitions = []
         for kernel_id, g in tqdm(grouped):
-            lst = g[['cell_index', 'cell_source', 'cell_num']].to_numpy()
-            prev_idx, prev_source, prev_num = lst[0]
-            for (idx, source, num) in lst[1:]:
-                code_changes = self._get_code_changes(prev_source, source) if idx == prev_idx else []
-                transitions.append({
-                    'kernel_id': kernel_id,
-                    'cell_idx_from': prev_idx, 'cell_num_from': prev_num, 'cell_source_from': prev_source,
-                    'cell_idx_to': idx, 'cell_num_to': num, 'cell_source_to': source,
-                    'inner_transition': idx == prev_idx,
-                    'changes': code_changes
-                })
-                prev_idx, prev_source, prev_num = idx, source, num
+            transitions += self.get_kernel_transitions(kernel_id, g)
 
         transitions_df = pd.DataFrame(transitions)
 
@@ -96,8 +106,10 @@ class MetricsProcessor:
              in self.cell_metrics_mapping[row.event].items()}
             for _, row in tqdm(df.iterrows())
         ]
-        metrics_df = pd.DataFrame(calculated_metrics).reset_index(drop=True)
-        resulted_df = pd.concat([df.reset_index(drop=True), metrics_df], axis=1)
+        metrics_df = pd.DataFrame(calculated_metrics)
+        resulted_df = pd.concat([
+            df.reset_index(drop=True), metrics_df.reset_index(drop=True)
+        ], axis=1)
 
         self.metrics_dataframes['cell_metrics'] = resulted_df
         return resulted_df
