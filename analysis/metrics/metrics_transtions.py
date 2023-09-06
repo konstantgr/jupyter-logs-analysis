1f52a87fc311b0188e1269471c5c8090c73c0b49
konstantgr <konstantin.grotov@gmail.com>
2023-09-06 17:30:57 +0200

    Add labels-transition analysis

--- analysis/metrics/metrics_transtions.py
+++ analysis/metrics/metrics_transtions.py
@@ -34,21 +34,22 @@ class TransitionMetrics(Metrics):
 
     def get_kernel_transitions(self, kernel_id: str, df: pd.DataFrame) -> List[Dict]:
         transitions = []
-        cols = ['cell_index', 'cell_source', 'cell_num']
+        cols = ['cell_index', 'cell_source', 'cell_num', 'label']
         executions_list = df[cols].to_numpy()
 
-        prev_idx, prev_source, prev_num = executions_list[0]
-        for (idx, source, num) in executions_list[1:]:
+        prev_idx, prev_source, prev_num, prev_label = executions_list[0]
+        for (idx, source, num, label) in executions_list[1:]:
             code_changes = self._get_code_changes(prev_source, source) if idx == prev_idx else []
             transitions.append({
                 'kernel_id': kernel_id,
                 'cell_idx_from': prev_idx, 'cell_num_from': prev_num, 'cell_source_from': prev_source,
                 'cell_idx_to': idx, 'cell_num_to': num, 'cell_source_to': source,
+                'cell_label_from': prev_label, 'cell_label_to': label,
                 'inner_transition': idx == prev_idx,
                 'changes': code_changes,
                 'type': 'execution_transition'
             })
-            prev_idx, prev_source, prev_num = idx, source, num
+            prev_idx, prev_source, prev_num, prev_label = idx, source, num, label
 
         return transitions
 
